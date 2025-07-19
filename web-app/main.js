f170be46624c2442b688232414de56b17346a99a
HarryWarriner <harry@warriner.org>
2025-07-19 14:35:12 +0100

    player scoring

--- web-app/main.js
+++ web-app/main.js
@@ -2,7 +2,9 @@ const smallNum = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10]
 const bigNum = [25,50,75,100]
 let outputNumbers =[]
 let numBig = 2
+let playerScore = 0
 
+// Getting random numbers for game setup
 function getRandomSmall() {
     randomSmall = smallNum[Math.floor(Math.random() * smallNum.length)];
     return randomSmall;
@@ -16,12 +18,29 @@ function getAimNum() {
     return Math.floor(Math.random() * (999 - 100) + 100);
 }
 
+// Picking an amount of big and amount of small numbers:
+
 for (let b = 0; b < numBig; b++) {
-    outputNumbers.push(getRandomBig())
+    outputNumbers.push(getRandomBig());
 }
 for (let i = 0; i < (6 - numBig); i++) {
-  outputNumbers.push(getRandomSmall())
+  outputNumbers.push(getRandomSmall());
 }
 
-console.log("Aim:", getAimNum())
+goalValue = getAimNum();
+
+// end game check, and score
+
+if (playerValue === goalValue) {
+    playerScore += 10
+} else if(Maths.abs(playerValue - goalValue) <= 10){
+    playerScore += 7
+} else {
+    
+}
+
+
+
+
+console.log("Aim:", goalValue)
 console.log("Numbers:", outputNumbers)
